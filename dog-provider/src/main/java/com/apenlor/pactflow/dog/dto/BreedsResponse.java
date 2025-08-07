11b2ecc1bd6479144cf615ad04be389385396fc2
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2025-08-07 17:10:23 +0300

    Update BreedsResponse.java

--- dog-provider/src/main/java/com/apenlor/pactflow/dog/dto/BreedsResponse.java
+++ dog-provider/src/main/java/com/apenlor/pactflow/dog/dto/BreedsResponse.java
@@ -1,7 +1,5 @@
 package com.apenlor.pactflow.dog.dto;
 
-import com.apenlor.pactflow.dog.entities.Breeds;
-
 import java.util.List;
 
 public record BreedsResponse(List<String> message, String status) {
