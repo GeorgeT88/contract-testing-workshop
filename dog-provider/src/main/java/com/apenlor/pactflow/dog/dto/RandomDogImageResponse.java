09a5d3a34be9b0c1577dd873678b1d5ca5d0a32a
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2025-08-11 10:42:35 +0300

    Update RandomDogImageResponse.java

--- dog-provider/src/main/java/com/apenlor/pactflow/dog/dto/RandomDogImageResponse.java
+++ dog-provider/src/main/java/com/apenlor/pactflow/dog/dto/RandomDogImageResponse.java
@@ -2,7 +2,7 @@ package com.apenlor.pactflow.dog.dto;
 
 import java.util.List;
 
-public record RandomDogImageResponse(String message, int status) {
+public record RandomDogImageResponse(String message, String status) {
 };
 
 
